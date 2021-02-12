c717ad80d7c99c5801b7980450d2a815d2801ebb
Gary Miguel <garymm@gmail.com>
2021-02-11 18:32:44 -0800

    no need for user-specific msmtprc

--- setup-debian.sh
+++ setup-debian.sh
@@ -58,5 +58,6 @@ cp oauth2tool.sh ~/bin/
 cp oauth2.py ~/bin/
 sudo cp msmtprc /usr/local/etc/msmtprc
 sudo ln -s /usr/local/etc/msmtprc /etc/msmtprc
-cp msmtprc ~/.msmtprc
+touch /var/log/msmtp
+chmod ugo+w /var/log/msmtp
 
