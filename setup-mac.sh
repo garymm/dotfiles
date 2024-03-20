8cd8c0f7e58eeee0762a2d1e5b68e6903920f4ca
Gary Miguel <garymm@garymm.org>
2024-03-20 10:32:04 -0700

    mac: give yabai passwordless sudo access

--- setup-mac.sh
+++ setup-mac.sh
@@ -107,3 +107,5 @@ cp -r mac/Library ~/
 curl --output ~/bin/git-pair --location https://raw.githubusercontent.com/cac04/git-pair/master/git-pair
 chmod +x ~/bin/git-pair
 
+# https://github.com/koekeishiya/yabai/wiki/Installing-yabai-(latest-release)#configure-scripting-addition
+echo "$(whoami) ALL=(root) NOPASSWD: sha256:$(shasum -a 256 $(which yabai) | cut -d " " -f 1) $(which yabai) --load-sa" | sudo tee /private/etc/sudoers.d/yabai
